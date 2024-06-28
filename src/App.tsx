2d7217c4ed3e0f18d689e1ce3757c993ad5c2d95
emmanueluwa <emmanuel@fulodev.com>
2024-06-28 10:05:49 +0100

    adding ability to update state of titleand description

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import React, { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
 
+// let title = "";
 const App = () => {
+  const [values, setValues] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <div className="max-w-3xl mx-auto space-y-6">
       <div className="space-y-6 bg-white shadow-md rounded p-5 ">
@@ -10,16 +24,27 @@ const App = () => {
             className="w-full border-b-2 border-gray-700 outline-none"
             type="text"
             placeholder="Title"
+            onChange={handleChange}
+            value={values.title}
+            name="title"
           />
         </div>
         <div>
           <textarea
             placeholder="Description"
             className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
+            value={values.description}
+            onChange={handleChange}
+            name="description"
           ></textarea>
         </div>
         <div className="text-center">
-          <button className="bg-red-500 text-white px-5 py-2 rounded">
+          <button
+            onClick={() => {
+              console.log(values);
+            }}
+            className="bg-red-500 text-white px-5 py-2 rounded"
+          >
             Add
           </button>
         </div>
