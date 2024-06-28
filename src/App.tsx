e9d47aed78913918842cf4b2a0878d4f65c60dcf
emmanueluwa <emmanuel@fulodev.com>
2024-06-28 05:38:42 +0100

    adding ui for note

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 const App = () => {
   return (
-    <div className="">
-      <h1 className="font-semibold text-2xl text-red-400">Noted</h1>
+    <div className="max-w-3xl mx-auto bg-white shadow-md rounded p-5 space-y-4">
+      <h1 className="font-semibold text-2xl text-center">Noted</h1>
       <div>
-        <input className="" type="text" placeholder="Title" />
+        <input
+          className="w-full border-b-2 border-gray-700 outline-none"
+          type="text"
+          placeholder="Title"
+        />
       </div>
       <div>
-        <textarea placeholder="Description" className=""></textarea>
+        <textarea
+          placeholder="Description"
+          className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
+        ></textarea>
+      </div>
+      <div className="text-center">
+        <button className="bg-red-500 text-white px-5 py-2 rounded">Add</button>
       </div>
     </div>
   );
